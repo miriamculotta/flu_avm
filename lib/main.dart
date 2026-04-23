8298ae4938f2772cd56b52e484e699cf29730cca
Miriam Culotta <mculott2@alumno.upv.es>
2026-04-23 18:35:55 +0200

    mculott2: Pantalla Domus con ListView

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flu_avm/presentation/screens/domus/domus_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: DomusScreen(),
+      );
+  }
+}
