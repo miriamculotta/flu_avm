9db1ea390a195b1206c7f8f5b6e3e504a1a1281f
Miriam Culotta <MCULOTT2@upv.edu.es>
2026-04-29 19:50:08 +0200

    mculott2: StatefulWidget para el contador

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,15 +4,14 @@ import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
-  routes: [
+  routes: <RouteBase>[
     GoRoute(
       path: '/', 
       builder: (context, state) => const DomusScreen(),
       ),
-      GoRoute(path: '/numerator-river', builder: (context, state) => const NumeratorScreen()
-      ),
-      GoRoute(path: '/bands', builder: (context, state) => const NumeratorScreen()
-      ),
-  ]
+      GoRoute(path: '/numerator-river', 
+      builder: (context, state) =>  const NumeratorScreen()
+      ),     
+  ],
 );
       
--- lib/presentation/screens/numerator/numerator_screen.dart
+++ lib/presentation/screens/numerator/numerator_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+/*
 class NumeratorScreen extends StatelessWidget {
-  const NumeratorScreen({super.key});
+
+  int counter = 0;
+
+  NumeratorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +14,49 @@ class NumeratorScreen extends StatelessWidget {
         title: Text('Numerator Screen'),
       ),
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge),
+        child: Text('Valor: $counter', style: Theme.of(context).textTheme.titleLarge),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter++;
+
+          print(counter);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+} 
+*/
+
+class NumeratorScreen extends StatefulWidget {
+  const NumeratorScreen({super.key});
+
+  @override
+  State<NumeratorScreen> createState() => _NumeratorScreenState();
+}
+
+class _NumeratorScreenState extends State<NumeratorScreen> {
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Numerator Screen'),
+      ),
+      body: Center(
+        child: Text('Valor: $counter', style: Theme.of(context).textTheme.titleLarge),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
