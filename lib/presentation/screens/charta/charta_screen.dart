2b134915399b2ee4612d2b93dbf8476077f318f1
Miriam Culotta <MCULOTT2@upv.edu.es>
2026-05-14 14:49:28 +0200

    mculott2: Formulario del Mapa

--- lib/presentation/screens/charta/charta_screen.dart
+++ lib/presentation/screens/charta/charta_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flu_avm/presentation/widgets/complere_form.dart';
 import 'package:flutter/material.dart';
 
 class ChartaScreen extends StatefulWidget {
@@ -13,6 +14,29 @@ class _ChartaScreenState extends State<ChartaScreen> {
     return Scaffold(
       appBar : AppBar(
         title: Text('Mapas'),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            ColoredBox(
+              color: Colors.blueGrey,
+              child: Center(
+                child: Text(
+                  'Mapa a pantalla completa',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                  )
+              )
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: ComplereForm(),
+
+                  ),
+                )
+          ],
+
         ),
        );
     }
