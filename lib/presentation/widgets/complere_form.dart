e1a2d3fbae13b40981176cfe2ee77503a70df50c
Miriam Culotta <MCULOTT2@upv.edu.es>
2026-05-14 15:50:52 +0200

    mculott2: Color y nombre en StateProviders

--- lib/presentation/widgets/complere_form.dart
+++ lib/presentation/widgets/complere_form.dart
@@ -1,11 +1,15 @@
 
+import 'package:flu_avm/config/config.dart';
+import 'package:flu_avm/presentation/providers/charta_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ComplereForm extends StatelessWidget {
+
+class ComplereForm extends ConsumerWidget {
   const ComplereForm({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
 
 
@@ -31,7 +35,7 @@ class ComplereForm extends StatelessWidget {
             children: [
               TextField(
                 keyboardType: TextInputType.name,
-                onChanged: (value) => { },
+                onChanged: (value) => ref.read(formNomenProvider.notifier).state = value,
                 style:TextStyle(color: Colors.white, fontSize: 18),
                 decoration: InputDecoration(
                   hintText: 'Pon tu nombre',
@@ -87,7 +91,7 @@ class ComplereForm extends StatelessWidget {
               ),
               SizedBox(height: 16),
               FilledButton(
-                onPressed: () => { },
+                onPressed: () => _onFormSubmit(ref),
                 style: FilledButton.styleFrom(
                   backgroundColor: Colors.black45,
                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
@@ -100,4 +104,19 @@ class ComplereForm extends StatelessWidget {
       ),
     );
   }
-}
+
+  void _onFormSubmit(WidgetRef ref) {
+
+    final String nameValue = ref.read(formNomenProvider);
+    final Color color = ref.read(formColorProvider);
+    final String colorValue = exColorAdHex(color);
+
+    if ( nameValue.isEmpty ) return;
+    
+    // TODO: aqui pondremos el marcador 
+
+    print('name: $nameValue, color: $colorValue');
+
+   }
+
+} 
