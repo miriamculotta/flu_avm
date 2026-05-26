bb9c6c31bb9e76f2c59d2f27a59f1e7057e9bdb9
Nombre Apellido <alumno@gmail.com>
2026-05-26 18:21:14 +0200

    mculott2: Añadir header con título Flu Avm y modo claro/oscuro funcional

--- lib/presentation/screens/welcome/welcome_screen.dart
+++ lib/presentation/screens/welcome/welcome_screen.dart
@@ -1,15 +1,58 @@
+import 'package:flu_avm/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends ConsumerWidget {
   const WelcomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('WelcomeScreen'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [_HeaderSection(), SizedBox(height: 20)],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class _HeaderSection extends ConsumerWidget {
+  const _HeaderSection();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final estTenebrisModus = ref.watch(estTenebrisModusProvider);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: const [
+            Icon(Icons.data_object_rounded, size: 32),
+            SizedBox(width: 8),
+            Text(
+              'Flu Avm',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        IconButton(
+          onPressed: () {
+            ref.read(estTenebrisModusProvider.notifier).state =
+                !estTenebrisModus;
+          },
+          icon: Icon(
+            estTenebrisModus
+                ? Icons.dark_mode_outlined
+                : Icons.light_mode_outlined,
+          ),
+        ),
+      ],
     );
   }
 }
